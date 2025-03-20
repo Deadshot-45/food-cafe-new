2a689da28c77346a1a29ca2365aa929d10db2162
Deadshot-45 <sahumayank8179@gmail.com>
2025-03-20 17:09:53 +0530

    DataProvider addded

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
@@ -7,26 +8,31 @@ import Menu from "./pages/Menu";
 import Review from "./pages/Review";
 import Footer from "./components/Footer";
 import ScrollToTop from "./components/ScrollToTop";
+import Cart from "./components/Cart/Cart";
+import DataProvider from "./Context/DataProvider";
 
 function App() {
   return (
-    <Router>
-      <ScrollToTop/>
-      <div className="min-h-screen w-full bg-gray-50">
-        <Navbar />
-        <main className="pt-16">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/menu" element={<Menu />} />
-            <Route path="/review" element={<Review />} />
-            {/* Add other routes here */}
-          </Routes>
-        </main>
-        <Footer/>
-      </div>
-    </Router>
+    <DataProvider>
+      <BrowserRouter future={{ v7_startTransition: true , v7_relativeSplatPath:true}}>
+        <ScrollToTop />
+        <div className="min-h-screen w-full bg-gray-50">
+          <Navbar />
+          <main className="pt-16">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/menu" element={<Menu />} />
+              <Route path="/review" element={<Review />} />
+              <Route path="/cart" element={<Cart />} />
+              {/* Add other routes here */}
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </BrowserRouter>
+    </DataProvider>
   );
 }
 
